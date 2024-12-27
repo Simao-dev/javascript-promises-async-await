279330ed62984c4c4c5be129db0444394a0f4e26
Simao-dev <pedrosimaocontato@gmail.com>
2024-12-27 17:37:07 -0300

    adicionado o carregamento de imagens e a pré-visualização das mesmas

--- scripts.js
+++ scripts.js
@@ -9,11 +9,33 @@ function lerConteudoDoArquivo(arquivo) {
     return new Promise((resolve, reject) => {
         const leitor = new FileReader();
         leitor.onload = () => {
-            resolve({ url: leitor.result, nome: arquivo.nome })
+            resolve({ url: leitor.result, nome: arquivo.name })
         }
 
         leitor.onerror = () => {
-            reject (`Erro na leitura do arquivo ${arquivo.nome}`)
+            reject (`Erro na leitura do arquivo ${arquivo.name}`)
+         
         }
+
+        leitor.readAsDataURL(arquivo)
     })
+
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        
+        } catch (erro) {
+            console.log("Erro na leitura do arquivo")
+        }
+    }
+})
